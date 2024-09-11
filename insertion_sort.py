363b9d47f8777e9609a6533cf21f34fd1760ac9e
iambikash378 <077bei015.bikash@pcampus.edu.np>
2024-09-11 17:14:40 +0545

    insertion sort and all

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,7 +2,7 @@ def insertion(list):
   listlen = len(list)
   for i in range(1,listlen):
     for j in range(i, 0, -1):
-      if list[j] < list[j-1]:
+      if list[j] > list[j-1]:
         swap = list[j]
         list[j] = list[j-1]
         list[j-1] = swap
@@ -11,5 +11,5 @@ def insertion(list):
   return list
 
 
-list = [5,4,3,2,1]
+list = [1,2,3,4,5]
 print(insertion(list))
